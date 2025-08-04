718f5169ad2125ea1525f7585362405cad4b2316
Damian Zabagło <damian.zabaglo@microsoft.wsei.edu.pl>
2025-08-04 23:03:12 +0200

    w

---
+++ StockMarketApi/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockMarketApi.Models;
+
+namespace StockMarketApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
--- StockMarketApi/Program.cs
+++ StockMarketApi/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using StockMarketApi.Data;
+
 namespace StockMarketApi
 {
     public class Program
@@ -13,7 +16,8 @@ namespace StockMarketApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
